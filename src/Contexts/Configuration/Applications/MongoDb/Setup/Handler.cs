5c075f543d7a48fe4cfb3532572dba4251b3f098
Charles Solar <charlessolar@gmail.com>
2018-05-22 01:59:59 -0500

    Change root seed to activate on Seeded event not command so the Setup entity exists

--- src/Contexts/Configuration/Applications/MongoDb/Setup/Handler.cs
+++ src/Contexts/Configuration/Applications/MongoDb/Setup/Handler.cs
@@ -14,7 +14,8 @@ namespace eShop.Configuration.Setup
         IHandleMessages<Events.Seeded>,
         IHandleMessages<Entities.Catalog.Events.Seeded>,
         IHandleMessages<Entities.Ordering.Events.Seeded>,
-        IHandleMessages<Entities.Identity.Events.Seeded>
+        IHandleMessages<Entities.Identity.Events.Seeded>,
+        IHandleMessages<Entities.Basket.Events.Seeded>
     {
         public async Task Handle(Queries.Status query, IMessageHandlerContext ctx)
         {
@@ -49,6 +50,13 @@ namespace eShop.Configuration.Setup
         {
             var model = await ctx.App<Infrastructure.IUnitOfWork>().Get<Models.ConfigurationStatus>("setup").ConfigureAwait(false);
             model.SetupContexts = model.SetupContexts.TryAdd("identity");
+
+            await ctx.App<Infrastructure.IUnitOfWork>().Update("setup", model).ConfigureAwait(false);
+        }
+        public async Task Handle(Entities.Basket.Events.Seeded e, IMessageHandlerContext ctx)
+        {
+            var model = await ctx.App<Infrastructure.IUnitOfWork>().Get<Models.ConfigurationStatus>("setup").ConfigureAwait(false);
+            model.SetupContexts = model.SetupContexts.TryAdd("basket");
 
             await ctx.App<Infrastructure.IUnitOfWork>().Update("setup", model).ConfigureAwait(false);
         }
--- src/Contexts/Configuration/Domain/Handlers/Setup/Handler.cs
+++ src/Contexts/Configuration/Domain/Handlers/Setup/Handler.cs
@@ -11,17 +11,23 @@ using StructureMap;
 namespace eShop.Configuration.Setup
 {
     public class Handler :
-        IHandleMessages<Commands.Seed>
+        IHandleMessages<Commands.Seed>,
+        IHandleMessages<Events.Seeded>
     {
         public async Task Handle(Commands.Seed command, IMessageHandlerContext ctx)
         {
             var setup = await ctx.For<Setup>().New("setup").ConfigureAwait(false);
             setup.Seed();
-
-            await ctx.SendToSelf(new Entities.Identity.Commands.Seed()).ConfigureAwait(false);
-            await ctx.SendToSelf(new Entities.Catalog.Commands.Seed()).ConfigureAwait(false);
-            await ctx.SendToSelf(new Entities.Basket.Commands.Seed()).ConfigureAwait(false);
-            await ctx.SendToSelf(new Entities.Ordering.Commands.Seed()).ConfigureAwait(false);
+        }
+        public async Task Handle(Events.Seeded e, IMessageHandlerContext ctx)
+        {
+            await ctx.LocalSaga(async bus =>
+            {
+                await bus.CommandToDomain(new Entities.Identity.Commands.Seed()).ConfigureAwait(false);
+                await bus.CommandToDomain(new Entities.Catalog.Commands.Seed()).ConfigureAwait(false);
+                await bus.CommandToDomain(new Entities.Basket.Commands.Seed()).ConfigureAwait(false);
+                await bus.CommandToDomain(new Entities.Ordering.Commands.Seed()).ConfigureAwait(false);
+            }).ConfigureAwait(false);
         }
     }
 }
